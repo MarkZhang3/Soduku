37487814e3fe455fb1e236d774eb65f70e86cd3d
unknown <341096105@gapps.yrdsb.ca>
2022-06-22 16:19:38 -0400

    finish

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,100 +1,149 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Line2D;
 
 
-public class MainWindow implements ActionListener {
-    public SudokuGame game;
+public class MainWindow extends JFrame{
+    // fields
+    public static SudokuGame game = new SudokuGame();
+    public static final int WIDTH = 700, HEIGHT = 750; // height and width used for the screen
+    private static Timer timer;
+
     public MainWindow()  {
 // Deceleration of components
-        JFrame Frame = new JFrame();
         JPanel gamePanel = new JPanel();
         JPanel actionPanel = new JPanel();
-        JLabel Label = new JLabel("/t/t/t/t/t/tWelcome to Game");
-        JButton NewGame =  new JButton("New Game");
-        JButton Rule =  new JButton("Rule");
-        JButton Solve =  new JButton("Solve");
-        game = new SudokuGame();
+        JButton newGame =  new JButton("New Game");
+        JButton rules =  new JButton("Rules");
+        JButton solve =  new JButton("Solve");
 
 
-// Frame
-        Frame.setTitle("Suduku Game");
-        Frame.setSize(700,750);
-        Frame.setDefaultCloseOperation(Frame.EXIT_ON_CLOSE);
-        Frame.setLocationRelativeTo(null);
+// frame
+        setTitle("Suduku Game");
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        //add game
         game.setSize(800, 800);
 
 
 
 
-// Panel
-        //ActionPanel
+// Panels
+        //ActionPanel (where the buttons will go)
 
         actionPanel.setLayout(new GridLayout(1,3));
         actionPanel.setBackground(Color.GRAY);
 
         //GamePanel
-
-        gamePanel.setLayout(new GridLayout(10,5));
         gamePanel.setBackground(Color.DARK_GRAY);
 
 
 // Button
-        NewGame.setFont(new Font("Arial", Font.BOLD, 18)); //NewGame
+        newGame.setFont(new Font("Arial", Font.BOLD, 18)); //NewGame button
 
-        Solve.setFont(new Font("Arial", Font.BOLD, 18));   //Solve
+        solve.setFont(new Font("Arial", Font.BOLD, 18));   //Solve button
 
-        Rule.setFont(new Font("Arial", Font.BOLD, 18));    //Rule
+        rules.setFont(new Font("Arial", Font.BOLD, 18));    //Rule button
 
-        Rule.addActionListener(this);
+// ActionListeners for the buttons
+        rules.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Rule r = new Rule();
+            }
+        });
 
-        NewGame.addActionListener(this);
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                getNewGame();
+            }
+        });
 
-        Solve.addActionListener(this);
+        solve.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Thread(()->{
+                    MainWindow.game.solve(MainWindow.game.getGrid(), 0, 0);
+                }).start();
+            }
+        });
+
+
 
 
 
 // Adding components
 
 
-        actionPanel.add(NewGame);
-        actionPanel.add(Rule);
-        actionPanel.add(Solve);
-        Frame.add(actionPanel,BorderLayout.SOUTH);
-        Frame.add(gamePanel, BorderLayout.CENTER);
-        Frame.add(game);
-        Frame.setVisible(true);
-
-
-
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JButton clickedButton = (JButton)e.getSource();
-//        switch(clickedButton.getText()) {
-//            case "Solve":
-//
-//        }
-        if (clickedButton.getText() == "Solve") {
-            Timer timer = new Timer(1, new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    game.repaint();
-                }
-            });
-            game.solve(game.getGrid(), 0, 0, timer);
-        }
-
-        //Rule rule = new Rule();
-
+        actionPanel.add(newGame);
+        actionPanel.add(rules);
+        actionPanel.add(solve);
+        add(actionPanel,BorderLayout.SOUTH);
+        add(gamePanel, BorderLayout.CENTER);
+        add(game);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-        MainWindow window = new MainWindow();
+        MainWindow m = new MainWindow();
+        // Refresh screen at approx 60Hz
+        timer = new Timer(16, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                game.repaint();
+            }
+        });
     }
 
+    /*
+    Draws lines to the JFrame (so that the lines can go over the JPanel)
+    All lines are drawn using ratios so when screen is resized, the lines will still be in the correct places
+     */
+    public void paint(Graphics g) {
+        super.paint(g);
+        // get current size (in case user resized window)
+        int h = getHeight();
+        int w = getWidth();
 
+        // Line2D needs Graphics2D to draw it
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        // make the line thicker so its visible
+        g2.setStroke(new BasicStroke(8f));
+        // makes a line and draws it to JFrame
+        Line2D line = new Line2D.Double(0.336*w, 40, 0.336*w, h-45);
+        g2.draw(line);
+        Line2D line2 = new Line2D.Double(0.663*w, 40, 0.663*w, h-45);
+        g2.draw(line2);
+        Line2D line3 = new Line2D.Double(2, 0.3488*h, w-2, 0.3488*h);
+        g2.draw(line3);
+        Line2D line4 = new Line2D.Double(2, 0.6444*h, w-2, 0.6444*h);
+        g2.draw(line4);
+    }
+
+    /*
+    param: none
+    return: none
+    Removes current game from frame
+    Generates a new game by calling SudokuGame constructor
+    Re-adds the new game tp frame
+     */
+    public void getNewGame() {
+        super.remove(game);
+        game = new SudokuGame();
+        game.setSize(800, 800);
+        super.add(game);
+        super.setVisible(true);
+        repaint();
+        // change up the sizes so that it redraws the lines
+        int w = getWidth(), h = getHeight();
+        setSize(400, 400);
+        setSize(w,h);
+    }
 
 }
 
--- src/SudokuGame.java
+++ src/SudokuGame.java
@@ -2,16 +2,36 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
 import java.util.concurrent.TimeUnit;
-
-public class SudokuGame extends JPanel {
+/**
+Sources used:
+ - Java API on JFrame, JPanel, GridLayout, ActionListener, JTextField, Line2D
+ - StackOverflow used for the following:
+        - adding thickness to Line2D: https://stackoverflow.com/questions/11030775/line2d-thickness-too-thick
+        - repainting JFrame and JPanel: https://stackoverflow.com/questions/22072796/how-to-repaint-a-jpanel-every-x-seconds
+        - resizing JLabels: https://stackoverflow.com/questions/6714045/how-to-resize-jlabel-imageicon
+ - Links that I didn't use, but referenced:
+        - swing timer: https://stackoverflow.com/questions/1006611/java-swing-timer
+        - making a thread: https://stackoverflow.com/questions/12551514/create-threads-in-java-to-run-in-background
+ - Special thanks to Mr.Benum and Adam Chen for helping with errors that occurred, ie. resizing the window to display lines on the JFrame
+    as well as displaying "solving" animation to JFrame by using thread and swing timer
+ - Sudoku Game, by Mark Zhang and Tom Wang
+ - ICS4U, Mr. Benum
+ - 2022/06/20
+ */
+public class SudokuGame extends JPanel implements Runnable{
     // fields
     private static final int SIZE = 9; // max length size
     public Box[][] grid = new Box[SIZE][SIZE]; // grid to store values of all Boxes on the 9x9 grid
+
     public SudokuGame() {
         super();
 
-        super.setLayout(new GridLayout(SIZE, SIZE)); // make the JFrame a grid layout that is 9x9
+        super.setLayout(new GridLayout(9,9)); // make the JFrame a grid layout that is 9x9
 
         // temporarily fill it with boxes
         for (int i = 0; i < SIZE; i ++) {
@@ -70,47 +90,97 @@ public class SudokuGame extends JPanel {
             }
         }
 
+        // add the boxes to the JPanel
         for (Box[] boxes : grid) {
-            for (Box b: boxes)  {
+            for (Box b : boxes) {
                 super.add(b);
             }
         }
-        printGrid(grid);
+
+        //printGrid(grid);
         super.setVisible(true);
     }
 
+    /**
+    paints component using super()
+     */
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
     }
 
+    /**
+    @param : none
+    @return : 2d box array
+    accessor method to return the grid
+     */
     public Box[][] getGrid() {
         return grid;
     }
 
+    /**
+    separate thread to repaint the JPanel so that the solving animation updates
+     @param : none
+     @return : none
+     @throws : none, but catches InterruptedException
+     */
+    @Override
+    public void run() {
+        try{
+            while (true) {
+                this.repaint();
+                Thread.sleep(1);
+            }
+        } catch(InterruptedException e) {
 
-    // An action listener for each box
-    public class BoxListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            Box sourceBox = (Box)e.getSource(); // gets the box that was clicked on
-            int numEntered = Integer.parseInt(sourceBox.getText()); // gets integer
-            System.out.println(numEntered + " " + sourceBox.row + " " + sourceBox.col);
-            paintBox(getGrid(), sourceBox.row, sourceBox.col, numEntered); // change the box accordingly
-            grid[sourceBox.row][sourceBox.col].changeNum(numEntered); // update the grid with made changes
         }
     }
+
+
+    /**
+     BoxListener is an action listener for each box
+     */
+    public class BoxListener implements ActionListener {
+        /**
+        @param : an action event (enter key)
+        @return : none
+        @throws : if string was entered in the box (I used Integer.parseInt, so strings are not handled)
+        gets the box that was clicked on and the current text inside the box
+        if statement handles if the user took back their number (no text inside box)
+        or otherwise take in number in the box and paint the box based on if its valid
+         */
+        public void actionPerformed(ActionEvent e) {
+            Box sourceBox = (Box)e.getSource(); // gets the box that was clicked on
+            int r = sourceBox.row;
+            int c = sourceBox.col;
+            if (sourceBox.getText().equals("")) {
+                grid[r][c].changeNum(0);
+                grid[r][c].setBackground(Color.WHITE); // reset
+            } else {
+                int numEntered = Integer.parseInt(sourceBox.getText()); // gets integer
+                paintBox(getGrid(), r, c, numEntered); // change the box accordingly
+                grid[r][c].changeNum(numEntered); // update the grid with made changes
+            }
+        }
+    }
+
+    /**
+    @param : 2d box array, current row and col (int)
+    @return : a true or false based on whether or not a solution has been found
+    getSolution will fill the grid such that each row and col of the array will hold a number
+    that is a valid sudoku solution and return a true or false on whether a solution has been completed
+     */
     public boolean getSolution(Box[][] grid, int row, int col) {
         // base case
         // if all the rows and cols have been filled, then the board is finished
         // * note that I set col to one higher than row, this is because of the next if statement
-        if (col >= 9 && row >= 8) {
+        if (col >= SIZE && row >= SIZE-1) {
             return true;
         }
 
         // for comprehensive and typing efficiency
         // it'll be faster if I just automatically call col+1 for everything, then
         // check at the beginning if col > 9, I go to next row, set col to 0
-        if (col > 8) {
+        if (col > SIZE-1) {
             col = 0;
             row ++;
         }
@@ -125,7 +195,6 @@ public class SudokuGame extends JPanel {
                 // temporarily change the current grid[row][col]
                 // and check if a solution can be found
                 grid[row][col].changeNum(trialNum);
-                //printGrid(grid);
                 if (getSolution(grid, row, col+1)) {
                     return true;
                 }
@@ -135,7 +204,9 @@ public class SudokuGame extends JPanel {
         return false;
     }
 
-    /*
+    /**
+    @param : a 2d array of Box, the current row and col and number that the user wants to check
+    @return : a true or false based on whether or not the number entered for the col and row in the grid is valid or not
     checks if the current number has been repeated in the row, column and 3x3 box it's in
      */
     public boolean valid(Box[][] grid, int row, int col, int num) {
@@ -165,10 +236,15 @@ public class SudokuGame extends JPanel {
         return true;
     }
 
-    // prints the 2d array
+    /**
+    @param grid that needs to be printed
+    @return none
+    prints the 2d array
+    this method is mostly for debugging purposes
+    */
     public void printGrid(Box[][] grid) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 System.out.print(grid[i][j].getNum()+" ");
             }
             System.out.println();
@@ -176,12 +252,15 @@ public class SudokuGame extends JPanel {
         System.out.println();
     }
 
-    /*
+    /**
+    @param : takes the 2d array storing all the boxes, the row, col that needs to be painted, and the number in the row&col
+    @return : none
     paints the boxes that have been filled out with corresponding colours:
     RED for a wrong guess
     GREEN for a guess that works based on the CURRENT state of the board
      */
-    public void paintBox(Box[][] grid, int row, int col, int num)  {
+    public void paintBox(Box[][] grid, int row, int col, int num) {
+        //grid = this.grid;
         if (valid(grid, row, col, num)) {
             grid[row][col].setBackground(Color.GREEN);
             //System.out.println('p');
@@ -190,56 +269,45 @@ public class SudokuGame extends JPanel {
             //System.out.println('l');
         }
     }
-
-    /*
+    /**
+    @param : 2d array that stores current boxes, the current row and col
+    @return : true or false based on whether or not a solution has been found
+    throws: none, but catches InterruptedException error
     Same as method getSolution, except this one is for visual
     I added a time sleeper and called paintBox for this method so that the
     user can see the algorithm in action
      */
-    public boolean solve(Box[][] grid, int row, int col, Timer timer){
+    public boolean solve(Box[][] grid, int row, int col){
         try {
-            if (col >= 9 && row >= 8) {
+            if (col >= SIZE && row >= SIZE-1) {
                 return true;
             }
 
-            if (col > 8) {
+            if (col > SIZE-1) {
                 col = 0;
                 row++;
             }
 
             if (grid[row][col].getNum() != 0) {
-                return solve(grid, row, col + 1, timer);
+                return solve(grid, row, col + 1);
             }
 
             for (int trialNum = 1; trialNum <= 9; trialNum++) {
                 paintBox(grid, row, col, trialNum);
-//                this.revalidate();
-//                this.repaint();
-                timer.start();
                 Thread.sleep(1);
                 if (valid(grid, row, col, trialNum)) {
                     grid[row][col].changeNum(trialNum);
-                    //printGrid(grid);
-                    if (solve(grid, row, col + 1, timer)) {
+                    if (solve(grid, row, col + 1)) {
                         return true;
                     }
                 }
             }
-            grid[row][col].changeNum(0); // backtrack
+            grid[row][col].changeNum(0);
             paintBox(grid, row, col, 0);
-//            revalidate();
-//            repaint();
-            timer.start();
             return false;
         } catch (InterruptedException e) {
 
         }
         return false;
     }
-//    @Override
-//    public void run() {
-//        while (true) {
-//            this.repaint();
-//        }
-//    }
 }
